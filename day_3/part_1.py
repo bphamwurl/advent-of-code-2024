b5c819d642c09b75387ffb8dce5aa9e0bbedadf7
benphamwurl <ben.pham@wurl.com>
2024-12-12 23:55:58 -0800

    Day 3

---
+++ day_3/part_1.py
@@ -0,0 +1,12 @@
+import re
+
+file = 'test.txt'
+
+with open(file,'r') as input:
+    data = [x.strip() for x in input.readlines()]
+    text = ''.join(data)
+
+    multi_pairs = re.findall(r'mul\((\d*),(\d*)\)', text)
+
+    print(sum([int(x)*int(y) for x,y in multi_pairs]))
+
---
+++ day_3/part_2.py
@@ -0,0 +1,23 @@
+import re
+
+file = 'input.txt'
+
+with open(file,'r') as input:
+    data = [x.strip() for x in input.readlines()]
+    text = ''.join(data)
+
+    multi_pairs = re.findall(r'mul\((\d*),(\d*)\)|(do\(\)|don\'t\(\))', text)
+
+    sum = 0
+    enabled = True
+    for num_1,num_2,instruction in multi_pairs:
+        if instruction == 'don\'t()':
+            enabled = False
+        
+        elif instruction == 'do()':
+            enabled = True
+        
+        elif enabled:
+            sum += int(num_1)*int(num_2)
+
+    print(sum)
