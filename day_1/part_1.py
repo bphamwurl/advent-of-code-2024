cfe323a8028e90a8a750029331e5b5d14dffe1de
benphamwurl <ben.pham@wurl.com>
2024-12-12 23:55:43 -0800

    Day 1

---
+++ day_1/part_1.py
@@ -0,0 +1,15 @@
+
+
+list_1 = list()
+list_2 = list()
+with open('input.txt','r') as input:
+    for row in input:
+        row = row.strip()
+        l1,l2 = row.split('  ')
+        list_1.append(int(l1))
+        list_2.append(int(l2))
+
+list_1.sort()
+list_2.sort()
+
+print(sum([abs(x-y) for x,y in zip(list_1,list_2)]))
---
+++ day_1/part_2.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+list_1 = list()
+list_2_count = defaultdict(int)
+with open('input.txt','r') as input:
+    for row in input:
+        l1,l2 = row.strip().split('  ')
+        list_1.append(int(l1))
+        list_2_count[int(l2)] += 1
+
+print(sum([num*list_2_count[num] for num in list_1]))
+
